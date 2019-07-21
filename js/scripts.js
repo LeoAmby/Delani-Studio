82993699cbed6bc7d88994d7b609c6878932d1a1
Leo <leoamby@gmail.com>
2019-07-21 23:29:10 +0300

    create divs for the hover texts meant for the portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -15,5 +15,15 @@ $(document).ready(function() {
         $("#product").show();
         $(".hidden").toggle();
         $("showing").toggle();
-      });  
+      }); 
+      
+      $(".img1").hover (function() {
+        $(this).stop().animate({opacity:.2},50);
+    $('.hover1').fadeIn();
+
+    }, function() {
+      $(this).stop().animate({opacity:1},500)
+      $('.hover1').fadeOut();
+});
+    
   });
