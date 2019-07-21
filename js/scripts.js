98b0117c9b4d863089fcb817babc469301a0ba79
Leo <leoamby@gmail.com>
2019-07-22 00:17:18 +0300

    enhance the jquery hovering effects on each portfolio image

--- js/scripts.js
+++ js/scripts.js
@@ -16,14 +16,78 @@ $(document).ready(function() {
         $(".hidden").toggle();
         $("showing").toggle();
       }); 
-      
-      $(".img1").hover (function() {
-        $(this).stop().animate({opacity:.2},50);
-    $('.hover1').fadeIn();
 
-    }, function() {
-      $(this).stop().animate({opacity:1},500)
-      $('.hover1').fadeOut();
-});
-    
+
+      // jquery hovering effects on the portfolio images
+      $(".img1").hover (function() {
+        $(this).stop().show();
+        $('.hover1').fadeIn();
+
+      }, function() {
+        $(this).stop().show();
+        $('.hover1').fadeOut();
+      });
+      
+      $(".img2").hover (function() {
+        $(this).stop().animate();
+        $('.hover2').fadeIn();
+
+      }, function() {
+        $(this).stop().animate()
+        $('.hover2').fadeOut();
+      });
+
+      $(".img3").hover (function() {
+        $(this).stop().animate();
+        $('.hover3').fadeIn();
+
+      }, function() {
+        $(this).stop().animate();
+        $('.hover3').fadeOut();
+      });
+
+      $(".img4").hover (function() {
+        $(this).stop().animate();
+        $('.hover4').fadeIn();
+
+      }, function() {
+        $(this).stop().animate()
+        $('.hover4').fadeOut();
+      });
+
+      $(".img5").hover (function() {
+        $(this).stop().animate();
+        $('.hover5').fadeIn();
+
+      }, function() {
+        $(this).stop().animate()
+        $('.hover5').fadeOut();
+      });
+
+      $(".img6").hover (function() {
+        $(this).stop().animate();
+        $('.hover6').fadeIn();
+
+      }, function() {
+        $(this).stop().animate()
+        $('.hover6').fadeOut();
+      });
+
+      $(".img7").hover (function() {
+        $(this).stop().animate();
+        $('.hover7').fadeIn();
+
+      }, function() {
+        $(this).stop().animate()
+        $('.hover7').fadeOut();
+      });
+
+      $(".img8").hover (function() {
+        $(this).stop().animate();
+        $('.hover8').fadeIn();
+
+      }, function() {
+        $(this).stop().animate()
+        $('.hover8').fadeOut();
+      });
   });
